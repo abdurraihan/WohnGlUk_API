f46e96706b2a0422d531e11ca81f33cf255e168d
abdurraihan <abdurraihan898@gmail.com>
2025-02-24 02:52:37 +0600

    connect with DB

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_STRING)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
- = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
