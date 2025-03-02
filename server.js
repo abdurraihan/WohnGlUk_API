6a5ab7f8c9c62910347c75f5fd12ec8795dbafe3
abdurraihan <abdurraihan898@gmail.com>
2025-03-03 02:12:40 +0600

    set a middleware for global error handling and make a utils for coustom error

--- server.js
+++ server.js
@@ -25,3 +25,15 @@ app.listen(PORT, () => {
 //use routers
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// Global Error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
