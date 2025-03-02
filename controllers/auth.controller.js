9eda59888c3cea2f908d81ba5b5ed6fdd3329c8d
abdurraihan <abdurraihan898@gmail.com>
2025-03-03 01:53:02 +0600

    make hashing and error handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,16 @@
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
+  const hashedPassword = await bcryptjs.hash(password, 10);
 
-  const newUser = new User({ username, email, password });
-  await newUser.save();
+  const newUser = new User({ username, email, password: hashedPassword });
 
-  res.status(201).json("user created successfully");
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
