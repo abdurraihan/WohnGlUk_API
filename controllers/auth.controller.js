a584781d1ba19f0c570fe9b010a0cfbce48b9544
abdurraihan <abdurraihan898@gmail.com>
2025-03-10 03:18:20 +0600

    make api for google OAuth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,14 +6,14 @@ import { errorhandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcryptjs.hash(password, 10);
-
+  console.log("hit");
   const newUser = new User({ username, email, password: hashedPassword });
   console.log("hiting signUp route");
   try {
     await newUser.save();
     res.status(201).json("user created successfully");
   } catch (error) {
-    next();
+    next(error);
   }
 };
 
@@ -22,6 +22,7 @@ export const sinin = async (req, res, next) => {
 
   try {
     const user = await User.findOne({ email });
+
     if (!user) {
       return next(errorhandler(404, "user not fount try with valid email"));
     }
@@ -30,12 +31,48 @@ export const sinin = async (req, res, next) => {
       return next(errorhandler(404, "wrond password Try again"));
     }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    console.log(token);
-
+    const { password: pass, ...rest } = user._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(user);
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  console.log("hit google");
+  const { name, email, photo } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassward = Math.random().toString(36).slice(-8);
+      const hashedPassword = await bcryptjs.hash(generatedPassward, 10);
+      const newUser = new User({
+        username:
+          name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-2),
+        email,
+        password: hashedPassword,
+        avatar: photo,
+      });
+
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup, sinin } from "../controllers/auth.controller.js";
+import { google, signup, sinin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", sinin);
+router.post("/google", google);
 
 export default router;
