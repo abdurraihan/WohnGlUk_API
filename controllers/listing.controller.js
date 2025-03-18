8f4cf6a2eae89305bac02860b4d0f12cb6148fcb
abdurraihan <abdurraihan898@gmail.com>
2025-03-18 16:45:21 +0600

    create get user listing api

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -1,9 +1,23 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  console.log("hit-ing listing");
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "you can only view your won listing"));
+  }
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  getUserListing,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
-
+router.get("/user/:id", verifyToken, getUserListing);
 export default router;
