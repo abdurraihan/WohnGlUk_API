fb504b8cf5c3f7dcbdcf8eb27a18487ea44eaf88
abdurraihan <abdurraihan898@gmail.com>
2025-03-20 02:46:11 +0600

    creat search Api

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   createListing,
   deletListing,
+  getListing,
+  getListings,
+  getUser,
   getUserListing,
   updateListing,
 } from "../controllers/listing.controller.js";
@@ -13,5 +16,10 @@ router.post("/create", verifyToken, createListing);
 router.get("/user/:id", verifyToken, getUserListing);
 router.delete("/delete/:id", verifyToken, deletListing);
 router.patch("/update/:id", verifyToken, updateListing);
+//get single listing by id
+router.get("/get/:id", getListing);
+// for contact by email
+router.get("/get", getListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
