7a9e328c2ba6a46c6525f45e9d3b46938f3e9b16
abdurraihan <abdurraihan898@gmail.com>
2025-04-03 01:10:13 +0600

    try the code deploy with jwt again

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -8,28 +8,18 @@ import {
   getUserListing,
   updateListing,
 } from "../controllers/listing.controller.js";
-// import { verifyToken } from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-// router.post("/create", verifyToken, createListing);
-// router.get("/user/:id", verifyToken, getUserListing);
-// router.delete("/delete/:id", verifyToken, deletListing);
-// router.patch("/update/:id", verifyToken, updateListing);
-// //get single listing by id
-// router.get("/get/:id", getListing);
-// // for contact by email
-// router.get("/get", getListings);
-// router.get("/:id", verifyToken, getUser);
-
-router.post("/create", createListing);
-router.get("/user/:id", getUserListing);
-router.delete("/delete/:id", deletListing);
-router.patch("/update/:id", updateListing);
+router.post("/create", verifyToken, createListing);
+router.get("/user/:id", verifyToken, getUserListing);
+router.delete("/delete/:id", verifyToken, deletListing);
+router.patch("/update/:id", verifyToken, updateListing);
 //get single listing by id
 router.get("/get/:id", getListing);
 // for contact by email
 router.get("/get", getListings);
-router.get("/:id", getUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import { verifyToken } from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
@@ -12,8 +12,8 @@ import {
 
 router.get("/test", test);
 
-// router.post("/update/:id", verifyToken, updateUser);
-// router.delete("/delete/:id", verifyToken, deleteUser);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 router.post("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
